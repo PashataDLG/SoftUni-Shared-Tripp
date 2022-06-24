de2dce88a2f86b3996ca0810485b11337a1c85c1
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2022-06-24 23:53:48 +0300

    Shared Trip and Details page rendering added

--- src/controllers/tripController.js
+++ src/controllers/tripController.js
@@ -14,10 +14,24 @@ router.post('/create', async function (req, res) {
     try {
         await tripService.createTrip(req.body);
 
-        res.redirect('/');
+        res.redirect('/trip/shared-trips');
     } catch (err) {
         res.render('trip/trip-create', { error: err.message });
     }
+});
+
+router.get('/shared-trips', async function (req, res) {
+    const trips = await tripService.getAll().lean();
+
+    res.render('trip/shared-trips', { trips });
+});
+
+router.get('/:tripId/details', async function (req, res) {
+    const trip = await tripService.getOneDetailed(req.params.tripId).lean();
+
+    const isOwner = trip.creator._id == req.user?._id;
+
+    res.render('trip/trip-details', { trip, isOwner });
 });
 
 module.exports = router;
--- src/services/tripService.js
+++ src/services/tripService.js
@@ -1,3 +1,7 @@
 const Trip = require('../models/Trip');
 
 exports.createTrip = (tripData) => Trip.create(tripData);
+
+exports.getAll = () => Trip.find();
+
+exports.getOneDetailed = (tripId) => Trip.findOne({ _id: tripId }).populate('creator');
